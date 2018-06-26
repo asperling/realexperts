b2e0ef8649279921b384df57eaadfcb61f96e2b7
Achim Wolf Sperling <achim.sperling@bike24.net>
2018-06-26 09:49:14 +0200

    Define fields for front page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,8 @@
 import CMS from 'netlify-cms'
 
-import FrontPagePreview from './preview-templates/FrontPagePreview'
+//import FrontPagePreview from './preview-templates/FrontPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('frontpage', FrontPagePreview)
+//CMS.registerPreviewTemplate('frontpage', FrontPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
